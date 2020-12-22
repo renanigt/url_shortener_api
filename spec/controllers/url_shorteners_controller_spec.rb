ce558d3aad3fdb57bad660f32d17e2b4d51c1200
renanigt <renanigt@gmail.com>
2020-12-22 01:38:37 -0300

    ad more tests to controller

--- spec/controllers/url_shorteners_controller_spec.rb
+++ spec/controllers/url_shorteners_controller_spec.rb
@@ -8,9 +8,20 @@ describe Api::V1::UrlShortenersController, type: :controller do
       allow(SecureRandom).to receive(:hex).and_return('80e1df8d')
     end
 
-    it 'returns shortened url' do
+    it 'creates a new ShortenedUrl' do
+      expect {
+        post :create, params: { url: url_test }
+      }.to change { ShortenedUrl.count }.by(1)
+    end
+
+    it 'returns ShortenedUrl' do
       post :create, params: { url: url_test }
       expect(response.body).to eq('http://localhost:3000/80e1df8d')
+    end
+
+    it 'calls UrlShortenerService' do
+      expect(UrlShortenerService).to receive(:short).with(url_test)
+      post :create, params: { url: url_test }
     end
 
     it 'returns status 201 (created)' do
@@ -20,17 +31,19 @@ describe Api::V1::UrlShortenersController, type: :controller do
   end
 
   describe 'GET show' do
+    let(:token) { '242572b9' }
+
     before do
-      ShortenedUrl.create!(original_url: url_test, token: '242572b9')
+      ShortenedUrl.create!(original_url: url_test, token: token)
     end
 
     it 'returns the original_url' do
-      get :show, params: { token: '242572b9' }
+      get :show, params: { token: token }
       expect(response.body).to eq(url_test)
     end
 
     it 'returns status 200 (success)' do
-      get :show, params: { token: '242572b9' }
+      get :show, params: { token: token }
       expect(response.status).to eq(200)
     end
   end
